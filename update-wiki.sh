9abd28691fb8590552686b35cbd6d6bfdf1b8666
OrlovM <orlov@email.com>
2021-04-25 14:07:22 +0300

    Changed sh

--- update-wiki.sh
+++ update-wiki.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+TEMP_WIKI_FOLDER="temp_wiki_$GITHUB_SHA"
+
 #Get commit details
 author=`git log -1 --format="%an"`
 email=`git log -1 --format="%ae"`
 message=`git log -1 --format="%s"`
 
+#Clone wiki repo
+git clone https://github.com/$GITHUB_REPO.wiki.git $TEMP_WIKI_FOLDER
+
 #Copy edited wiki 
-cp -r $1/. wiki-repo
+cp -r $1/. $TEMP_WIKI_FOLDER
 
 #Check if wiki has changes
-cd wiki-repo
+cd $TEMP_WIKI_FOLDER
 if git diff-index --quiet HEAD; then
   echo "Nothing changed"
   return
